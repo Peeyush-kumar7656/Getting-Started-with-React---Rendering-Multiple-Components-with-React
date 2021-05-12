0eba157347ea8d7972f4b35504bb5ce079ce9a11
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-12 18:38:28 +0530

    modify

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-     <h2 data-ns-test = "project-name">Weather App</h2>
-     <p data-ns-test = "project-description">weather app also provides atmospheric pressure, weather conditions, visibility distance, relative humidity, precipitation in different unites, dew point, wind speed and direction, 
+     <h2 data-ns-test="project-name">Weather App</h2>
+     <p data-ns-test="project-description">weather app also provides atmospheric pressure, weather conditions, visibility distance, relative humidity, precipitation in different unites, dew point, wind speed and direction, 
      in addition to ten days in future and hourly weather forecast.</p>
-     <h2 data-ns-test = "project-name">Build a Social Media App</h2>
-     <p data-ns-test = "project-description">Twitter, Facebook, and Instagram are quite sophisticated, and include an ever-growing number of features to keep users engaged. On top of that, it's the kind of app we likely know best regarding how it should function.</p>
+     <h2 data-ns-test="project-name">Build a Social Media App</h2>
+     <p data-ns-test="project-description">Twitter, Facebook, and Instagram are quite sophisticated, and include an ever-growing number of features to keep users engaged. On top of that, it's the kind of app we likely know best regarding how it should function.</p>
     </div>
   )
 }
